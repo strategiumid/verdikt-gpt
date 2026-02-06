cc09209a0187e314fb444504979617e5291b204e
strategiumid <nikolskiiaics@gmail.com>
2026-02-06 11:34:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ class VerdiktChatApp {
         // Конфигурация API
         this.API_CONFIG = {
             url: 'https://openrouter.ai/api/v1/chat/completions',
-            model: 'tngtech/deepseek-r1t2-chimera:free',
+            model: 'sourceful/riverflow-v2-pro',
             apiKey: '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',
             maxTokens: 2000,
             temperature: 0.7
@@ -1394,4 +1394,5 @@ class VerdiktChatApp {
 
 // Создаем глобальный экземпляр для доступа из HTML
 window.VerdiktChat = new VerdiktChatApp();
+
 
