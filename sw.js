8f3c7a5a319ffe0c70d4fa1eefbe83056e6fa5c9
strategiumid <nikolskiiaics@gmail.com>
2026-02-06 12:34:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-// Простой Service Worker для офлайн работы
-self.addEventListener('install', function(event) {
-    console.log('Service Worker установлен');
-});
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            console.log('Офлайн режим');
-            return new Response('Офлайн режим');
-        })
-    );
-});
