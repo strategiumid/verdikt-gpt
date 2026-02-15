5de37547b1b6ab29c27b870b721443a1242577ed
strategiumid <nikolskiiaics@gmail.com>
2026-02-15 19:35:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ export class VerdiktChatApp {
         this.API_CONFIG = {
             url: 'https://routerai.ru/api/v1/chat/completions',
             model: 'stepfun/step-3.5-flash', 
-            maxTokens: 1500,
+            maxTokens: 1600,
             temperature: 0.8,
             apiKey: "sk-ayshgI6SUUplUxB0ocKzEQ1IK73mbdql"
         };
